90c364a7aecfc5cffbeab263b8b66611532182af
zYuna646 <anggarangga646@gmail.com>
2024-08-30 04:56:19 +0800

    fix migration from vite to mix

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,6 @@
-import './bootstrap';
+// import './bootstrap';
+require('./bootstrap.js');
+
 import Alpine from 'alpinejs'
  
 window.Alpine = Alpine
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,10 @@
 		<title>{{ config('app.name') }}{{ isset($title) ? " | $title" : '' }}</title>
 		<link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-		@vite(['resources/css/app.css', 'resources/js/app.js'])
+		{{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+		<link rel="stylesheet" href="{{ mix('css/app.css') }}">
+		<script src="{{ mix('js/app.js') }}" defer></script>
+
 		@stack('styles')
 		<style>
 			[x-cloak] {
