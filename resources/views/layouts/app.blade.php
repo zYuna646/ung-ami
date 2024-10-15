8906359dd6bc281856ebe74103be6ee117546bc5
zYuna646 <anggarangga646@gmail.com>
2024-10-15 21:02:19 +0800

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,8 +9,8 @@
 		<link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 		{{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
-		<link rel="stylesheet" href="{{ mix('css/app.css') }}">
-		<script src="{{ mix('js/app.js') }}" defer></script>
+		<link rel="stylesheet" href="{{ asset('css/app.css') }}">
+		<script src="{{ asset('js/app.js') }}" defer></script>
 
 		@stack('styles')
 		<style>
