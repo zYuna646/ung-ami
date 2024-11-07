631ce890b2959a8cd7e317fbf262343823b5be88
fachiri <fachrycooles@gmail.com>
2024-11-07 09:49:14 +0800

    feat: add required attribute

--- resources/views/components/form/textarea.blade.php
+++ resources/views/components/form/textarea.blade.php
@@ -1,10 +1,11 @@
 @props([
-	'class' => '',
-	'name',
-	'label' => null,
-	'placeholder' => '',
-	'inputClass' => '',
-	'value' => old($name),
+    'class' => '',
+    'name',
+    'label' => null,
+    'placeholder' => '',
+    'inputClass' => '',
+    'value' => old($name),
+    'required' => false,
 ])
 <div class="{{ $class }}">
 	@if ($label)
@@ -13,10 +14,11 @@
 		</label>
 	@endif
 	<textarea
-		id="{{ $name }}"
-		name="{{ $name }}"
-		placeholder="{{ $placeholder }}"
-		{{ $attributes->class(["bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-purple-500 focus:border-purple-500 block w-full p-2.5", $inputClass, 'border-red-700' => $errors->get($name)]) }}
+	 id="{{ $name }}"
+	 name="{{ $name }}"
+	 placeholder="{{ $placeholder }}"
+	 {{ $attributes->class(['bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-purple-500 focus:border-purple-500 block w-full p-2.5', $inputClass, 'border-red-700' => $errors->get($name)]) }}
+	 {{ $required ? 'required' : '' }}
 	>{{ $value }}</textarea>
 	@error($name)
 		<p class="mt-2 text-xs text-red-600">
